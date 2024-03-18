9ed51f3108243cf86fb1809bc5a7c1abdb06481c
Gabriel Souza <contato@devgabrielsouza.com.br>
2024-03-17 22:10:15 -0300

    :lipstick: Adjust character list columns for better UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -145,7 +145,7 @@ export default function Home() {
                 </SearchForm>
             </div>
 
-            <div className="grid cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 ">
+            <div className="grid cols-1 xl:cols-2 gap-4 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 ">
                 {characters &&
                     characters.map((character: Character) => (
                         <Link
