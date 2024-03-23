c1ca556ae21fa3fb6c691faed1897c9828c92829
Gabriel Souza <contato@devgabrielsouza.com.br>
2024-03-23 17:18:24 -0300

    :recycle: Refactor to avoid duplicate validation logic

--- src/data/usecases/remote-getCharacters.ts
+++ src/data/usecases/remote-getCharacters.ts
@@ -1,34 +1,38 @@
-import { HttpClient, HttpStatusCode } from '../protocols'
+import { HttpClient, HttpResponse, HttpStatusCode } from '../protocols'
 import { GetCharacters } from '@/lib/characters'
 import { Character } from '@/types/Character'
 
+export function handleResponse<T>(response: HttpResponse<T>): T {
+    switch (response.statusCode) {
+        case HttpStatusCode.ok:
+            return response.body
+        case HttpStatusCode.badRequest:
+            throw new Error(response.body.error || 'Bad Request')
+        case HttpStatusCode.notFound:
+            throw new Error(
+                response.body.error ||
+                    'Oops! The character you are looking for could not be found'
+            )
+        case HttpStatusCode.serverError:
+            throw new Error(response.body.error || 'Server Error')
+        case HttpStatusCode.unauthorized:
+            throw new Error(response.body.error || 'Unauthorized')
+        default:
+            throw new Error('Unexpected Error')
+    }
+}
 export class RemoteGetCharacters implements GetCharacters {
     constructor(
         private readonly url: string,
         private readonly httpClient: HttpClient<Character[]>
     ) {}
-    get = async () => {
+    get = async (): Promise<any> => {
         const httpResponse = await this.httpClient.request({
             url: this.url,
             method: 'get',
         })
 
-        switch (httpResponse.statusCode) {
-            case HttpStatusCode.ok:
-                return httpResponse.body
-            case HttpStatusCode.badRequest:
-                throw new Error('Bad Request')
-            case HttpStatusCode.notFound:
-                throw new Error(
-                    'Oops! The character you are looking for could not be found'
-                )
-            case HttpStatusCode.serverError:
-                throw new Error('Server Error')
-            case HttpStatusCode.unauthorized:
-                throw new Error('Unauthorized')
-            default:
-                throw new Error('Unexpected Error')
-        }
+        return handleResponse(httpResponse)
     }
 }
 
@@ -37,27 +41,12 @@ export class RemoteGetCharacter implements GetCharacters {
         private readonly url: string,
         private readonly httpClient: HttpClient<Character>
     ) {}
-    get = async () => {
+    get = async (): Promise<any> => {
         const httpResponse = await this.httpClient.request({
             url: this.url,
             method: 'get',
         })
 
-        switch (httpResponse.statusCode) {
-            case HttpStatusCode.ok:
-                return httpResponse.body
-            case HttpStatusCode.badRequest:
-                throw new Error('Bad Request')
-            case HttpStatusCode.notFound:
-                throw new Error(
-                    'Oops! The character you are looking for could not be found'
-                )
-            case HttpStatusCode.serverError:
-                throw new Error('Server Error')
-            case HttpStatusCode.unauthorized:
-                throw new Error('Unauthorized')
-            default:
-                throw new Error('Unexpected Error')
-        }
+        return handleResponse(httpResponse)
     }
 }
