60fc2fe37cfc43c1468a8a7d3769c325342df042
Gabriel Souza <contato@devgabrielsouza.com.br>
2024-03-22 21:10:45 -0300

    :bug: Fixes error handling on the single character page

--- src/app/character/[id]/page.tsx
+++ src/app/character/[id]/page.tsx
@@ -11,6 +11,9 @@ import { Badge } from '@/components/ui/badge'
 import Image from 'next/image'
 import BackButton from '@/components/ui/back-button'
 import { getStatusColor } from '@/components/ui/character-card'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import MainTitle from '@/components/ui/main-title'
 
 interface CharacterPageProps {
     params: {
@@ -21,86 +24,108 @@ interface CharacterPageProps {
 export default async function CharacterPage({
     params: { id: characterId },
 }: CharacterPageProps) {
-    const character = await getRemoteCharacter(characterId)
+    try {
+        const character = await getRemoteCharacter(characterId)
 
-    if (!character.name) {
+        if (!character.name) {
+            return (
+                <CharacterNotFound className="text-center text-2xl font-semibold h-screen flex-col gap-2 items-center justify-center">
+                    Character not found
+                    <BackButton />
+                </CharacterNotFound>
+            )
+        }
+
+        return (
+            <div className="max-w-2xl mx-auto my-10 p-6 bg-white rounded-lg shadow-md">
+                <CharacterHeader>
+                    <CharacterProfileImage>
+                        <Image
+                            src={character.image}
+                            className="rounded-full"
+                            width={200}
+                            height={200}
+                            alt={character.name}
+                        />
+                    </CharacterProfileImage>
+
+                    <h1 className="mt-4 text-3xl font-bold">
+                        {character.name}
+                    </h1>
+                    <CharacterBadges>
+                        <Badge
+                            variant="outline"
+                            className={`text-white ${getStatusColor(
+                                character.status
+                            )}`}
+                        >
+                            {character.status}
+                        </Badge>
+                        <Badge className="ml-2" variant="outline">
+                            {character.species}
+                        </Badge>
+                        <Badge className="ml-2" variant="outline">
+                            {character.gender}
+                        </Badge>
+                    </CharacterBadges>
+                </CharacterHeader>
+
+                <CharacterInfoSection>
+                    <h2 className="text-xl font-semibold">Origin</h2>
+                    <p>{character.origin.name}</p>
+                </CharacterInfoSection>
+                <CharacterInfoSection>
+                    <h2 className="text-xl font-semibold">
+                        Last Known Location
+                    </h2>
+                    <p>{character.location.name}</p>
+                </CharacterInfoSection>
+                <CharacterInfoSection>
+                    <h2 className="text-xl font-semibold">Episodes</h2>
+                    <div className="flex flex-wrap gap-2 mt-2">
+                        {character.episode.map((episode: string) => {
+                            const match = episode.match(/\d+$/)
+                            if (match) {
+                                const episodeNumber = match[0]
+                                return (
+                                    <Badge
+                                        className="w-20 bg-zinc-800 text-white text-center flex justify-center"
+                                        variant="outline"
+                                        key={episode}
+                                    >
+                                        {episodeNumber}
+                                    </Badge>
+                                )
+                            }
+                        })}
+                    </div>
+                </CharacterInfoSection>
+
+                <CharacterInfoSection>
+                    <h2 className="text-xl font-semibold">Created on</h2>
+                    <p>
+                        {new Date(character.created).toLocaleDateString(
+                            'en-US'
+                        )}
+                    </p>
+                </CharacterInfoSection>
+
+                <CharacterInfoSection>
+                    <BackButton />
+                </CharacterInfoSection>
+            </div>
+        )
+    } catch (error) {
         return (
             <CharacterNotFound className="text-center text-2xl font-semibold h-screen flex-col gap-2 items-center justify-center">
-                Character not found
-                <BackButton />
+                <MainTitle>Character not found</MainTitle>
+                <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+                    Oops! The character you are looking for could not be found.
+                </p>
+                <Link href="/">
+                    <Button variant="outline">Go back home</Button>
+                </Link>
             </CharacterNotFound>
         )
     }
-
-    return (
-        <div className="max-w-2xl mx-auto my-10 p-6 bg-white rounded-lg shadow-md">
-            <CharacterHeader>
-                <CharacterProfileImage>
-                    <Image
-                        src={character.image}
-                        className="rounded-full"
-                        width={200}
-                        height={200}
-                        alt={character.name}
-                    />
-                </CharacterProfileImage>
-
-                <h1 className="mt-4 text-3xl font-bold">{character.name}</h1>
-                <CharacterBadges>
-                    <Badge
-                        variant="outline"
-                        className={`text-white ${getStatusColor(
-                            character.status
-                        )}`}
-                    >
-                        {character.status}
-                    </Badge>
-                    <Badge className="ml-2" variant="outline">
-                        {character.species}
-                    </Badge>
-                    <Badge className="ml-2" variant="outline">
-                        {character.gender}
-                    </Badge>
-                </CharacterBadges>
-            </CharacterHeader>
-
-            <CharacterInfoSection>
-                <h2 className="text-xl font-semibold">Origin</h2>
-                <p>{character.origin.name}</p>
-            </CharacterInfoSection>
-            <CharacterInfoSection>
-                <h2 className="text-xl font-semibold">Last Known Location</h2>
-                <p>{character.location.name}</p>
-            </CharacterInfoSection>
-            <CharacterInfoSection>
-                <h2 className="text-xl font-semibold">Episodes</h2>
-                <div className="flex flex-wrap gap-2 mt-2">
-                    {character.episode.map((episode: string) => {
-                        const match = episode.match(/\d+$/)
-                        if (match) {
-                            const episodeNumber = match[0]
-                            return (
-                                <Badge
-                                    className="w-20 bg-zinc-800 text-white text-center flex justify-center"
-                                    variant="outline"
-                                    key={episode}
-                                >
-                                    {episodeNumber}
-                                </Badge>
-                            )
-                        }
-                    })}
-                </div>
-            </CharacterInfoSection>
-
-            <CharacterInfoSection>
-                <h2 className="text-xl font-semibold">Created on</h2>
-                <p>{new Date(character.created).toLocaleDateString('en-US')}</p>
-            </CharacterInfoSection>
-
-            <CharacterInfoSection>
-                <BackButton />
-            </CharacterInfoSection>
-        </div>
-    )
 }
